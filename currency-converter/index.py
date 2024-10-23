60fcaf12da7e2385e47b6e73046bb39f8e694ad1
touhid-imam <touhid.imam1337@gmail.com>
2024-10-23 02:25:59 -0500

    currency converter

---
+++ currency-converter/index.py
@@ -0,0 +1,44 @@
+def get_amount():
+    while True:
+        try:
+            amount = float(input("Enter the amount: "))
+            if amount <= 0:
+                raise ValueError
+            return amount
+        except ValueError:
+            print("Invalid Amount.")
+
+
+def get_currency(label='Source'):
+    currencies = ('USD', 'EUR', 'CAD')
+    while True:
+        currency = input(f'{label} Currency (USD/EUR/CAD): ').upper()
+        if currency not in currencies:
+            print("Invalid Currency")
+        else:
+            return currency
+
+
+def convert(amount, source_currency, target_currency):
+    exchange_rates = {
+        'USD': {'EUR': 0.85, 'CAD': 1.25},
+        'EUR': {'USD': 1.18, 'CAD': 1.47},
+        'CAD': {'USD': 0.80, 'EUR': 0.68},
+
+    }
+
+    if source_currency == target_currency:
+        return amount
+    return amount * exchange_rates[source_currency][target_currency]
+
+
+def main():
+    amount = get_amount()
+    source_currency = get_currency(label="Source")
+    target_currency = get_currency(label="Source")
+    converted_amount = convert(amount, source_currency, target_currency)
+    print(f'{amount} {source_currency} is equal to {converted_amount:.2f}')
+
+
+if __name__ == '__main__':
+    main()
